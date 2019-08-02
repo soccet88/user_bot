1710821ee171fa310f3d6ec9ba8726a40638c510
soccet_88 <47474355+soccet88@users.noreply.github.com>
2019-08-02 12:03:08 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var meme = require('./damp');
+
+
+client.on('ready', () => {
+    console.log('I am ready!');
+  });
+
+client.on('message', message => { 
+    if(message.content === "!анекдот") {
+        message.channel.send(meme.aneki());
+    }
+});
+
+client.on('message', message => { 
+    if(message.content === "!слава") {
+        message.channel.send('Украине');
+    }
+}); 
+
+client.on('message', message => { 
+    if(message.content === "!info") {
+        message.channel.send('Бот знает команды:\n - !слава\n - !анекдот\n - !паста\n - !iq\n - !мое фото ');
+    }
+});
+
+client.on('message', message => {
+    if(message.content === "!паста") {
+        message.channel.send(meme.pasti()); 
+    }
+});
+
+client.on('message', (msg) => {
+    if(msg.content === "!iq") {
+        msg.channel.send(`IQ  ${msg.author} ` + meme.iq()); 
+    }
+});
+
+client.on('message', (msg) => {
+    if(msg.content === "!мое фото") {
+        msg.channel.send(meme.shs()); 
+    }
+});
+
+
+client.login(process.env.BOT_TOKEN);
+
+
+
+
+
+
+
