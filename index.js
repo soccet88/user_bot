df64a3e436f8a0dd111410741310e820eb9fd58c
soccet_88 <47474355+soccet88@users.noreply.github.com>
2020-07-06 00:49:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -71,4 +71,10 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => { 
+    if(message.content === "!Павла") {
+        message.channel.send(meme.pavla());
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
